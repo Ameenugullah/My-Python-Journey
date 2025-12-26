262cddad596364bd276b4c3d45ef3bddd190e849
Your Namelear <you@example.com>
2025-12-26 08:33:53 +0100

    now

--- for-loop.py
+++ for-loop.py
@@ -1,13 +1,13 @@
 # For loops = execute a block of code a fixed number of times.
 #             You can iterate over a range, string, sequence, etc.
 
-#for x in reversed(range(1, 11)):
-#   print(x)
-#print("HAPPY NEW YEAR")
+for x in reversed(range(1, 11)):
+   print(x)
+print("HAPPY NEW YEAR")
 
-#credit_card = "1234-8750-8065-7890"
-#for x in credit_card:
-#    print(x)
+credit_card = "1234-8750-8065-7890"
+for x in credit_card:
+    print(x)
 
 for x in range(1, 30):
     if x == 15:
